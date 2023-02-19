b861e0db0abd354fb3dd5d1d4eecfdfee1b67a1e
Paul Burt <41005722+beebpeeb@users.noreply.github.com>
2023-02-19 16:46:58 +0000

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,66 @@
+use chrono::NaiveDateTime;
+use serde::{de::Error, Deserialize, Deserializer};
+
+const SUFFIX: &str = " e.";
+
+pub type Listings = Vec<Listing>;
+
+#[derive(Debug, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Listing {
+    description: String,
+    live: bool,
+    #[serde(deserialize_with = "de_datetime", rename = "startTime")]
+    start_time: NaiveDateTime,
+    title: String,
+}
+
+impl Listing {
+    pub fn date(&self) -> String {
+        self.start_time.format("%d.%m.%Y").to_string()
+    }
+
+    pub fn description(&self) -> &str {
+        self.description.trim().trim_end_matches(SUFFIX)
+    }
+
+    pub fn has_description(&self) -> bool {
+        !self.description.trim().is_empty()
+    }
+
+    pub fn is_live(&self) -> bool {
+        self.live
+    }
+
+    pub fn is_repeat(&self) -> bool {
+        self.description.trim().ends_with(SUFFIX)
+    }
+
+    pub fn time(&self) -> String {
+        self.start_time.format("%H:%M").to_string()
+    }
+
+    pub fn title(&self) -> &str {
+        self.title.trim()
+    }
+}
+
+#[derive(Deserialize)]
+struct APIResponse {
+    results: Listings,
+}
+
+pub async fn fetch_schedule() -> Result<Listings, Box<dyn std::error::Error>> {
+    let url = "https://apis.is/tv/ruv";
+    let res: APIResponse = reqwest::get(url).await?.json().await?;
+    Ok(res.results)
+}
+
+fn de_datetime<'de, D>(deserializer: D) -> Result<NaiveDateTime, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s: String = Deserialize::deserialize(deserializer)?;
+    let fmt = "%Y-%m-%d %H:%M:%S";
+    let dt = NaiveDateTime::parse_from_str(&s, fmt).map_err(D::Error::custom)?;
+    Ok(dt)
+}
---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use askama::Template;
+use axum::{routing::get, Router, Server};
+use std::net::SocketAddr;
+
+use dagskra::{fetch_schedule, Listings};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(index));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate<'a> {
+    date: String,
+    listings: Listings,
+    title: &'a str,
+}
+
+async fn index() -> IndexTemplate<'static> {
+    let listings = fetch_schedule().await.unwrap_or_default();
+    let date = listings.first().map_or_else(|| "".to_owned(), |l| l.date());
+    IndexTemplate {
+        date,
+        listings,
+        title: "Dagskrá RÚV",
+    }
+}
