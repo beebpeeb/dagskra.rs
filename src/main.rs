1d915e9520fcc0d6d2746f25aaddc36a3892c494
Paul Burt <41005722+beebpeeb@users.noreply.github.com>
2023-12-05 17:18:37 +0000

    Removed Shuttle

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,39 @@
 use askama::Template;
-use axum::{routing, Router};
-use shuttle_axum::ShuttleAxum;
-use tracing::info;
+use axum::{routing::get, serve, Router};
+use chrono::Utc;
+use tokio::net::TcpListener;
 
 use dagskra::{fetch_listings, Listing};
 
-#[shuttle_runtime::main]
-async fn axum_app() -> ShuttleAxum {
-    info!("Starting app");
-    let router = Router::new()
-        .route("/", routing::get(index))
-        .route("/_listings", routing::get(listings));
-    Ok(router.into())
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(index))
+        .route("/_listings", get(listings));
+    let listener = TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    serve(listener, app).await.unwrap();
 }
 
 #[derive(Template)]
 #[template(path = "index.html")]
 struct IndexTemplate {
+    date: String,
     title: &'static str,
 }
 
 async fn index() -> IndexTemplate {
     let title = "Dagskrá RÚV";
-    IndexTemplate { title }
+    let date = Utc::now().format("%d.%m.%Y").to_string();
+    IndexTemplate { date, title }
 }
 
 #[derive(Template)]
 #[template(path = "_listings.html")]
 struct ListingsTemplate {
-    date: String,
     listings: Vec<Listing>,
 }
 
 async fn listings() -> ListingsTemplate {
-    info!("Fetching schedule data");
     let listings = fetch_listings().await.unwrap_or_default();
-    let date = listings
-        .first()
-        .map_or_else(|| "".to_string(), |listing| listing.date());
-    ListingsTemplate { date, listings }
+    ListingsTemplate { listings }
 }
