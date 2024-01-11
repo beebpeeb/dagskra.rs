de98f5ce4627384a2f89f3e399ca3795c1dd4d92
Paul Burt <41005722+beebpeeb@users.noreply.github.com>
2024-01-11 09:53:48 +0000

    Date format doesn't need to be global

--- src/date_format.rs
+++ src/date_format.rs
@@ -1,13 +1,12 @@
 use chrono::NaiveDateTime;
 use serde::{self, Deserialize, Deserializer};
 
-const FORMAT: &str = "%Y-%m-%d %H:%M:%S";
-
 pub fn deserialize<'de, D>(deserializer: D) -> Result<NaiveDateTime, D::Error>
 where
     D: Deserializer<'de>,
 {
+    let fmt = "%Y-%m-%d %H:%M:%S";
     let s = String::deserialize(deserializer)?;
-    let dt = NaiveDateTime::parse_from_str(&s, FORMAT).map_err(serde::de::Error::custom)?;
+    let dt = NaiveDateTime::parse_from_str(&s, fmt).map_err(serde::de::Error::custom)?;
     Ok(dt)
 }
